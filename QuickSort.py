1a3e5bdc310f3541c82e0168c174a6e782ed162c
Ibrahim Malik <ibstar.malik@googlemail.com>
2018-07-03 10:31:03 +0100

    Add files via upload
    Quick sort without median of 3 or duplicate balancing optimisations.

---
+++ QuickSort.py
@@ -0,0 +1,50 @@
+
+def quicksort(data):
+
+    if len(data) < 2:
+        raise ValueError("Too few items to sort ")
+
+    else:
+        quicksorthelper(data, 0, len(data)-1)
+
+
+def quicksorthelper(data, lo, hi):
+    
+    if lo < hi:
+        splitpoint = partition(data, lo, hi)
+        quicksorthelper(data, lo, splitpoint-1)
+        quicksorthelper(data, splitpoint+1, hi)
+
+
+    
+def partition(data, lo, hi):
+    
+    pivotval = data[lo]
+    leftmark = lo+1
+    rightmark = hi
+    
+    complete = False
+    while not complete:
+
+        while data[leftmark] <= pivotval and leftmark <= rightmark: # why < rightmark instead of < hi
+            leftmark += 1
+        
+
+        while data[rightmark] >= pivotval and rightmark >= leftmark: # why > leftmark mark instead of > lo
+            rightmark -= 1
+
+        if rightmark < leftmark:
+            complete = True
+
+        else:
+            data[leftmark], data[rightmark] = data[rightmark], data[leftmark]
+
+    data[lo], data[rightmark] = data[rightmark], data[lo]
+    return rightmark
+
+    
+alist = [54,26,93,55,77,31,44,55,20]
+quicksort(alist)
+print(alist)
+
+
