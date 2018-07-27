936282c8de297d76e1b2580fff1334b750bc4c2c
Ibrahim Malik <32967336+IKMalik@users.noreply.github.com>
2018-07-27 15:01:52 +0100

    Update BinarySearchTree.py
    Fixed bugs added new functions for find min and completed hibbard deletion

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -4,9 +4,9 @@
 # 20/7/18 added inorder traversal to view nodes
 # 26/7/18 refactored naming conventions
 # 26/7/18 fixed delete min rec bug
-
-#binary search tree
-
+# 27/7/18 Added find min function and completed delete key function // hibbard deletion  
+# need to keep in mind scope and return nature in recursive functions
+# 27/7/18 Fixed insidious bug in find min // return bug 
 
 #binary search tree
 
@@ -29,18 +29,21 @@ class binarysearch():
         self.size = 0
         self.rootnode = None
 
+    def getroot(self):
+
+        return self.rootnode
+
+    def getsize(self):
+        print (self.rootnode.count)
+
     def insert(self, key, data):
 
         if self.rootnode is None:
             self.rootnode = anode(key, data)
         else:
-            self.insertnode(self.rootnode, key, data)
+            self._insert(self.rootnode, key, data)
 
-    def getroot(self):
-
-        return self.rootnode
-
-    def insertnode(self, node, key, data):
+    def _insert(self, node, key, data):
 
             if node.key == key:
                 node.data = data
@@ -49,17 +52,14 @@ class binarysearch():
                 if node.leftnode is None:                    
                     node.leftnode = anode(key, data)
                 else:
-                    self.insertnode(node.leftnode, key, data)
+                    self._insert(node.leftnode, key, data)
             else:
                 if node.rightnode is None:
                     node.rightnode = anode(key, data)
                 else:
-                    self.insertnode(node.rightnode, key, data)
+                    self._insert(node.rightnode, key, data)
 
             node.count = 1 + self.sizenode(node.leftnode) + self.sizenode(node.rightnode)
-
-    def getsize(self):
-        print (self.rootnode.count)
 
     def sizenode(self, node):
 
@@ -73,13 +73,13 @@ class binarysearch():
         if self.rootnode is None:
             return False
         else:
-            found = self.searchnode(self.rootnode, key)
+            found = self._searchkey(self.rootnode, key)
             if found:
                 return True
             else:
                 return False
 
-    def searchnode(self, node, key):
+    def _searchkey(self, node, key):
         
         if node is None:
             return False
@@ -89,10 +89,10 @@ class binarysearch():
                 return True
 
             elif key < node.key:
-                self.searchnode(node.leftnode, key)
+                self._searchkey(node.leftnode, key)
 
             else:
-                self.searchnode(node.rightnode, key)
+                self._searchkey(node.rightnode, key)
         
                 
     def floor(self, node, key): 
@@ -130,8 +130,16 @@ class binarysearch():
                 return node.key
             else:
                 return ceilkey
+            
+    def getrank(self, key): # number keys < key 
 
-    def ranknode(self, node, key): # number keys < key 
+        if self.rootnode is None:
+            return 0
+        
+        else:
+            print( self._getrank(self.rootnode, key))
+
+    def _getrank(self, node, key): 
 
         if node is None:
             return 0
@@ -141,51 +149,116 @@ class binarysearch():
             if node.key == key:
                 return self.sizenode(node.leftnode)
 
-            elif root.key > key:
-                return self.ranknode(node.leftnode, key) 
+            elif node.key > key:
+                return self._getrank(node.leftnode, key) 
                 
             else:
-                return 1 + self.sizenode(node.leftnode) + self.ranknode(node.rightnode, key)
-                
-    def getrank(self, key):
-
-        if self.rootnode is None:
-            return 0
-        else:
-            print( self.ranknode(self.rootnode, key))
+                return 1 + self.sizenode(node.leftnode) + self._getrank(node.rightnode, key)
+            
 
     def getnodes(self):
         
         if self.rootnode is None:
             print("No nodes exist")
+            
         else:
             print("getting nodes")
-            self.inorder(self.rootnode)
+            self._getnodes(self.rootnode)
 
 
-    def inorder(self, node):
+    def _getnodes(self, node):
 
         if node is not None:
 
-            self.inorder(node.leftnode)
+            self._getnodes(node.leftnode)
             print(node.key)
-            self.inorder(node.rightnode)
+            self._getnodes(node.rightnode)
+
+    def findmin(self):
+
+        if self.rootnode is not None:
+            print( self._findmin(self.rootnode).key)
+
+    def _findmin(self, node):
+
+        if node.leftnode is None:
+            return node
+        else:
+            return self._findmin(node.leftnode)
 
     def deletemin(self):
 
         if self.rootnode is None:
             print("No nodes exist")
+            
         else:
-            self.rootnode = self.deleteminnode(self.rootnode)
+            self.rootnode = self._deletemin(self.rootnode)
 
-    def deleteminnode(self, node):
+    def _deletemin(self, node):
 
         if node.leftnode is not None:
-            node.leftnode = self.deleteminnode(node.leftnode)
+            node.leftnode = self._deletemin(node.leftnode)
+            node.count = 1 + self.sizenode(node.leftnode) + self.sizenode(node.rightnode)
             return node
+        
         else:
             return node.rightnode
+
+    def deletekey(self, key):
+
+        if self.rootnode is not None:
+            self.rootnode = self._deletekey(self.rootnode, key)
+
+    def _deletekey(self, node, key):
+
+
+        if key > node.key:
+            node.rightnode = self._deletekey(node.rightnode, key)
+            
+        elif key < node.key:
+            node.leftnode = self._deletekey(node.leftnode, key)
+            
+        else:
+
+            if node.leftnode == None:
+                node = node.rightnode
+
+            elif node.rightnode == None:
+                node = node.leftnode
+
+            else:
+                tempnode = node
+                node = self._findmin(node.rightnode)
+                node.rightnode = self._deletemin(tempnode.rightnode)
+                node.leftnode = tempnode.leftnode
+                node.count = 1 + self.sizenode(node.leftnode) + self.sizenode(node.rightnode)
+                
+        return node
+
+'''
+if __name__ == '__main__':
+
+    a = binarysearch()
+    a.insert(7,7)
+    a.insert(1,1)
+    a.insert(0.5, 0.5)
+    a.insert(0.25,0.25)
+    a.insert(0.1, 0.1)
+    a.insert(8,8)
+    a.insert(3,3)
+    a.insert(9,9)
+    a.insert(2,2)
+    a.insert(4,4)
+    a.insert(3.5, 3.5)
+    a.insert(5,5)
+    a.insert(11,11)
+    a.insert(10,10)
+    a.deletekey(3)
+    a.getnodes()
+            
+
         
+'''
 
 if __name__ == '__main__':
 
