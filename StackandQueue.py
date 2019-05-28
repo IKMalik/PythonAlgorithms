d23d901b6244e57fda5e707d98fb50209b1e4e5c
Ibrahim Malik <32967336+IKMalik@users.noreply.github.com>
2019-05-28 15:14:13 +0100

    Update StackandQueue.py

--- StackandQueue.py
+++ StackandQueue.py
@@ -1,4 +1,4 @@
-#array implementation of stack
+#array implementation of stack and queue 
 
 class Stack():
 
@@ -23,3 +23,43 @@ class Queue():
     def dequeue(self):
         self.list_vals.pop(0)
         print(str(self.list_vals))
+        
+# Linked List implementation stack and queue 
+
+class Node:
+
+  def __init__(self, data):
+
+    self.data = data
+    self.nextnode = None
+
+class Stack:
+
+  def __init__(self):
+    
+    self.size = 0
+    self.headnode = None
+  
+  def push(self, data):
+
+    anode = Node(data)
+    if self.headnode is None:
+      self.headnode = anode
+    else:
+      anode.nextnode = self.headnode.nextnode
+      self.headnode = anode
+    self.size += 1
+
+  def pop(self):
+
+    if self.headnode is None:
+      return None
+    else:
+      data = self.headnode.data
+      self.headnode = self.headnode.nextnode
+      self.size -= 1
+      return data
+  
+  def size(self):
+
+    return self.size
