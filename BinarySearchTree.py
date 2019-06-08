f99986ace02d4d2e92f1d31c9dcb7aaf42480f1e
Ibrahim Malik <32967336+IKMalik@users.noreply.github.com>
2019-06-08 18:19:15 +0100

    Update BinarySearchTree.py

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -9,61 +9,46 @@
 # 27/7/18 Fixed insidious bug in find min // return bug 
 # 30/5/19 Fixed major bug in insert 
 # 6/6/19 Added put method as better alternative to insert
+# 8/6/19 Overhauled many methods fixing many bugs 
 
 #binary search tree
 
 
-class anode():
+class Node():
 
-    def __init__(self, key, data):
+  def __init__(self, key, value):
 
-        self.data = data
-        self.key = key
-        self.leftnode = None
-        self.rightnode = None
-        self.count = 1
+    self.key = key
+    self.value = value
+    self.count = 0
+    self.left = None
+    self.right = None
 
+class BST():
 
-class binarysearch():
+  def __init__(self):
 
-    def __init__(self):
+    self.root = None
+  
+  def get(self, key):
 
-        self.size = 0
-        self.rootnode = None
+    if self.root is None:
+      return None
+    else:
 
-    def getroot(self):
+      current = self.root
+      while (current != None):
 
-        return self.rootnode
-
-    def getsize(self):
-        print (self.rootnode.count)
-
-    def insert(self, key, data):
-
-        if self.rootnode is None:
-            self.rootnode = anode(key, data)
+        if key == current.key:
+          print('key found')
+          return current.value
+        elif key > current.key:
+          current = current.right
         else:
-            self.rootnode = self._insert(self.rootnode, key, data)
+          current = current.left
+      
+      return None
 
-    def _insert(self, node, key, data):
-
-            if node.key == key:
-                node.data = data
-                
-            elif key < node.key:
-                if node.leftnode is None:                    
-                    node.leftnode = anode(key, data)
-                else:
-                    self._insert(node.leftnode, key, data)
-            else:
-                if node.rightnode is None:
-                    node.rightnode = anode(key, data)
-                else:
-                    self._insert(node.rightnode, key, data)
-
-            node.count = 1 + self.sizenode(node.leftnode) + self.sizenode(node.rightnode)
-            return node
-        
   def put(self, key, value):
     
     self.root = self._put(self.root, key, value)
@@ -77,234 +62,121 @@ class binarysearch():
       node.value = value
 
     elif key > node.key:
-      node.right = self.put(node.right, key, value)
+      node.right = self._put(node.right, key, value)
 
     else:
-      node.left = self_put(node.left, key, value)
-
+      node.left = self._put(node.left, key, value)
+    
+    node.count = 1 + self._size(node.left) + self._size(node.right)
     return node
+  
+  def size(self):
 
-    def sizenode(self, node):
+    if self.root is None:
+      return None
+    else:
+      print ('size of root node is: ', self.root.count)
+      return 
+  
+  def _size(self, node):
 
-        if node is None:
-            return 0
-        else:
-            return node.count
+    if node is None:
+      return 0
+    else:
+      return node.count
 
-    def searchkey(self, key):
+  def floor(self, key):
 
-        if self.rootnode is None:
-            return False
-        else:
-            found = self._searchkey(self.rootnode, key)
-            if found:
-                return True
-            else:
-                return False
+    resultnode = self._floor(self.root, key)
+    if resultnode is None:
+      return None
+    else:
+      print('floor key is: ', resultnode.key)
+      return resultnode.key
+  
+  def _floor(self, node, key):
 
-    def _searchkey(self, node, key):
-        
-        if node is None:
-            return False
-            
-        else:
-            if key == node.key:
-                return True
-
-            elif key < node.key:
-                self._searchkey(node.leftnode, key)
-
-            else:
-                self._searchkey(node.rightnode, key)
-        
-                
-    def floor(self, node, key): 
-
-        if node is None:
-            return None
-
-        elif node.key == key:
-            return key
-
-        elif node.key > key:
-            return self.floor(node.leftnode, key)
-
-        else:
-            floorkey = self.floor(node.rightnode, key)
-            if (floorkey is None) or (floorkey > key):
-                return node.key 
-            else:
-                return floorkey
-
-    def ceil(self, node, key): 
-
-        if node == None:
-            return None
-
-        elif node.key == key:
-            return key
-
-        elif node.key < key:
-            return self.ceil(node.rightnode, key)
-
-        else:             
-            ceilkey = self.ceil(node.leftnode, key)
-            if (ceilkey is None) or (ceilkey < key):
-                return node.key
-            else:
-                return ceilkey
-            
-    def getrank(self, key): # number keys < key 
-
-        if self.rootnode is None:
-            return 0
-        
-        else:
-            print( self._getrank(self.rootnode, key))
-
-    def _getrank(self, node, key): 
-
-        if node is None:
-            return 0
-        
-        else:
-            
-            if node.key == key:
-                return self.sizenode(node.leftnode)
-
-            elif node.key > key:
-                return self._getrank(node.leftnode, key) 
-                
-            else:
-                return 1 + self.sizenode(node.leftnode) + self._getrank(node.rightnode, key)
-            
-
-    def getnodes(self):
-        
-        if self.rootnode is None:
-            print("No nodes exist")
-            
-        else:
-            print("getting nodes")
-            self._getnodes(self.rootnode)
-
-
-    def _getnodes(self, node):
-
-        if node is not None:
-
-            self._getnodes(node.leftnode)
-            print(node.key)
-            self._getnodes(node.rightnode)
-
-    def findmin(self):
-
-        if self.rootnode is not None:
-            print( self._findmin(self.rootnode).key)
-
-    def _findmin(self, node):
-
-        if node.leftnode is None:
-            return node
-        else:
-            return self._findmin(node.leftnode)
-
-    def deletemin(self):
-
-        if self.rootnode is None:
-            print("No nodes exist")
-            
-        else:
-            self.rootnode = self._deletemin(self.rootnode)
-
-    def _deletemin(self, node):
-
-        if node.leftnode is not None:
-            node.leftnode = self._deletemin(node.leftnode)
-            node.count = 1 + self.sizenode(node.leftnode) + self.sizenode(node.rightnode)
-            return node
-        
-        else:
-            return node.rightnode
-
-    def deletekey(self, key):
-
-        if self.rootnode is not None:
-            self.rootnode = self._deletekey(self.rootnode, key)
-
-    def _deletekey(self, node, key):
-
-
-        if key > node.key:
-            node.rightnode = self._deletekey(node.rightnode, key)
-            
-        elif key < node.key:
-            node.leftnode = self._deletekey(node.leftnode, key)
-            
-        else:
-
-            if node.leftnode == None:
-                node = node.rightnode
-
-            elif node.rightnode == None:
-                node = node.leftnode
-
-            else:
-                tempnode = node
-                node = self._findmin(node.rightnode)
-                node.rightnode = self._deletemin(tempnode.rightnode)
-                node.leftnode = tempnode.leftnode
-                node.count = 1 + self.sizenode(node.leftnode) + self.sizenode(node.rightnode)
-                
+    if node is None:
+      return None
+    
+    elif node.key == key:
+      return node 
+    
+    elif node.key > key:
+      return self._floor(node.left, key)
+      
+    else:
+      altnode = self._floor(node.right, key)
+      if altnode is None:
         return node
+      else:
+        return altnode
+  
+  def rank(self, key):
 
-'''
+    size = self._rank(self.root, key)
+    print('rank of key is: ', size )
+  
+  def _rank(self, node, key):
+
+    if node is None:
+      return 0
+    elif node.key == key:
+      return self._size(node.left)
+    elif node.key > key:
+      return self._rank(node.left, key)
+    else: 
+      return 1 + self._size(node.left) + self._rank(node.right, key)
+  
+  def delete(self, key):
+    
+    self.root = self._delete(self.root, key)
+
+  def _delete(self, node, key):
+
+    if node is None:
+      return None
+    elif key < node.key:
+      node.left = self._delete(node.left, key)
+    elif key > node.key:
+      node.right = self._delete(node.right, key)
+    # key found
+    else:
+      if node.left is None:
+        return node.right
+      if node.right is None:
+        return node.left
+      else:
+        tempnode = node
+        node = self.getchild(node.right)      
+        node.left = tempnode.left
+        node.right = self._delete(tempnode.right, node.key)
+    return node
+  
+  def getchild(self, node):
+    while node.left is not None:
+      node = node.left
+    return node
+  
+
 if __name__ == '__main__':
+  a = BST()
+  a.put(7,7)
+  a.put(3,3)
+  a.put(10,10)
+  a.put(1,1)
+  a.put(5,5)
+  a.put(8,8)
+  a.get(5)
+  a.get(10)
+  a.size()
+  a.floor(6)
+  a.floor(8)
+  a.rank(7)
+  a.rank(3)
+  a.rank(1)
+  a.delete(3)
+  a.get(3)
+  a.get(5)
 
-    a = binarysearch()
-    a.insert(7,7)
-    a.insert(1,1)
-    a.insert(0.5, 0.5)
-    a.insert(0.25,0.25)
-    a.insert(0.1, 0.1)
-    a.insert(8,8)
-    a.insert(3,3)
-    a.insert(9,9)
-    a.insert(2,2)
-    a.insert(4,4)
-    a.insert(3.5, 3.5)
-    a.insert(5,5)
-    a.insert(11,11)
-    a.insert(10,10)
-    a.deletekey(3)
-    a.getnodes()
-            
-
-        
-'''
-
-if __name__ == '__main__':
-
-    a = binarysearch()
-    a.insert(7,7)
-    a.insert(1,1)
-    a.insert(0.5, 0.5)
-    a.insert(8,8)
-    a.insert(3,3)
-    a.insert(9,9)
-    a.insert(2,2)
-    a.insert(4,4)
-    a.insert(11,11)
-    a.insert(10,10)
-    a.searchkey(7)
-    root = a.getroot()
-    ceil = a.ceil(root, 10)
-    print(ceil)
-    floor = a.floor(root, 10)
-    print(floor)
-    a.getsize()
-    a.getrank(8)
-    a.getnodes()
-    a.deletemin()
-    a.getnodes()
             
