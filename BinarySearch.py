52fedb1e9b1c3668269a575bdeae4a110d551529
Ibrahim Malik <32967336+IKMalik@users.noreply.github.com>
2019-06-13 21:11:48 +0100

    Update BinarySearch.py

--- BinarySearch.py
+++ BinarySearch.py
@@ -23,3 +23,27 @@ class BinarySearch:
       return self._search(0, mid-1, key)
     else:
       return self._search(mid+1, len(sel.data)-1, key)
+
+# ITERATIVE IMPLEMENTATION
+
+class BinarySearch:
+
+  def __init__(self, data):
+
+    self.data = data
+  
+  def search(self, key):
+
+    lo = 0
+    hi = len(self.data)-1
+    while hi > lo:
+
+      mid = (hi+lo)//2
+
+      if key == self.data[key]:
+        return True
+      elif key < self.data[key]:
+        hi = mid-1
+      else:
+        lo = mid+1 
+    return False
