a9420c7de4b3326e62276d87bd428959ffdf40c4
Ibrahim Malik <32967336+IKMalik@users.noreply.github.com>
2019-06-23 22:04:43 +0100

    Create LongestCommonSubseqDynamicProg.py

---
+++ LongestCommonSubseqDynamicProg.py
@@ -0,0 +1,39 @@
+def lcs(word1, word2):
+
+  x = len(word1)
+  y = len(word2)
+  return _lcs(word1, word2, x, y)
+
+def _lcs(word1, word2, x, y):
+
+  # create matrix 
+
+  matrix = [[-1]*(x) for val in range (0,y)]
+
+  for i in range(0, x):
+    for j in range(0, y):
+      
+      if word1[i] == word2[j]:
+        val1 = 0
+        val2 = 0
+        if i-1 >= 0:
+          val1 = matrix[i-1][j]
+        if j-1 >= 0:
+          val2 = matrix[i][j-1]
+        matrix[i][j] = 1 + max(val1,val2)
+
+      else:
+        val1 = 0
+        val2 = 0
+        if i-1 >= 0:
+          val1 = matrix[i-1][j]
+        if j-1 >= 0:
+          val2 = matrix[i][j-1]
+        matrix[i][j] = max(val1,val2)
+  
+  print(matrix)
+  return matrix[x-1][y-1]
+
+b = 'aword'
+a = 'wsrod'
+print(lcs(a,b))
