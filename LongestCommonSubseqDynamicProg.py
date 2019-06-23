ef497fc8ebb9ccdeec00d0c84abd4bd149515fa4
Ibrahim Malik <32967336+IKMalik@users.noreply.github.com>
2019-06-23 22:50:11 +0100

    Update LongestCommonSubseqDynamicProg.py

--- LongestCommonSubseqDynamicProg.py
+++ LongestCommonSubseqDynamicProg.py
@@ -1,4 +1,4 @@
-EDIT: MORE FUNTIONAL CODE
+EDIT: fixed bug counting repeated letters, e.g abb and abc would give 3 instead of 2
   
 def lcs(word1, word2):
 
@@ -12,55 +12,14 @@ def _lcs(word1, word2, x, y):
 
   matrix = [[-1]*(x) for val in range (0,y)]
 
-  for i in range(0, x):
-    for j in range(0, y):
+  for i in range(0, y):
+    for j in range(0, x):
       
-      if word1[i] == word2[j]:
-        matrix[i][j] = 1 + max(getmax(i-1, j, matrix), getmax(i, j-1, matrix))
-      else: 
-        matrix[i][j] = max(getmax(i-1, j, matrix), getmax(i, j-1, matrix))
-
-  return matrix[x-1][y-1]
-
-def getmax(a, b, matrix):
-
-  if a < 0 or b < 0 :
-    return 0
-  else:
-    return matrix[a][b]
-
-b = 'aword'
-a = 'wsrod'
-print(lcs(a,b))
-
-  
-  
-  
-  
-  
-def lcs(word1, word2):
-
-  x = len(word1)
-  y = len(word2)
-  return _lcs(word1, word2, x, y)
-
-def _lcs(word1, word2, x, y):
-
-  # create matrix 
-
-  matrix = [[-1]*(x) for val in range (0,y)]
-
-  for i in range(0, x):
-    for j in range(0, y):
-      
-      if word1[i] == word2[j]:
-        val1 = 0
-        val2 = 0
-        if i-1 >= 0:
-          val1 = matrix[i-1][j]
-        if j-1 >= 0:
-          val2 = matrix[i][j-1]
-        matrix[i][j] = 1 + max(val1,val2)
+      if word1[j] == word2[i]:
+        if i-1 < 0 or j-1 < 0:
+          matrix[i][j] = 1 
+        else:
+          matrix[i][j] = 1 + matrix[i-1][j-1]
 
       else:
         val1 = 0
@@ -71,9 +30,8 @@ def _lcs(word1, word2, x, y):
           val2 = matrix[i][j-1]
         matrix[i][j] = max(val1,val2)
   
-  print(matrix)
-  return matrix[x-1][y-1]
+  return matrix[y-1][x-1]
 
-b = 'aword'
-a = 'wsrod'
+a = 'ABC'
+b = 'ABCD'
 print(lcs(a,b))
