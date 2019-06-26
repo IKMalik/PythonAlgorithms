51a3ccca02b9d8861baa4f8329de7dbaa4c0da9e
Ibrahim Malik <32967336+IKMalik@users.noreply.github.com>
2019-06-26 17:29:12 +0100

    Update 01Knapsack.py

--- 01Knapsack.py
+++ 01Knapsack.py
@@ -1,3 +1,4 @@
+
 def knapsack(weights, values, maximum_weight):
 
   matrix = [ [0] * (1+maximum_weight) for _ in range( len(weights) ) ]
@@ -6,22 +7,14 @@ def knapsack(weights, values, maximum_weight):
 
     for total in range(maximum_weight+1):
 
-      # weight is possible inclusion
       if weights[weight] <= total:
 
-        if weight > 0:
-          matrix[weight][total] = max(values[weight] + matrix[weight-1][total-weights[weight]], matrix[weight-1][total])
+        matrix[weight][total] = values[weight] if weight == 0 else max(values[weight] + matrix[weight-1][total-weights[weight]], matrix[weight-1][total])
 
-        else:
-          matrix[weight][total] = values[weight]
-
-      # weight excluded 
       else:
           if weight > 0:
             matrix[weight][total] = matrix[weight-1][total]
 
-
-  print(matrix)
   return matrix[-1][-1]
 
 
