05edf114874db36ac65629cf71d666006ad1e071
Ibrahim Malik <32967336+IKMalik@users.noreply.github.com>
2019-07-07 18:30:39 +0100

    Update and rename mergesort.py to Mergesort.py
    added better code

---
+++ Mergesort.py
@@ -0,0 +1,33 @@
+
+def mergesort(data):
+
+    if len(data) <= 1:
+        return data
+    mid = len(data)//2
+    left = mergesort(data[:mid])
+    right = mergesort(data[mid:])
+    return merge(left, right)
+  
+def merge(left, right):
+
+    aux = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+      if left[i] <= right[j]:
+        aux.append(left[i])
+        i += 1
+      else:
+        aux.append(right[j])
+        j += 1
+    
+    if i < len(left):
+        aux += left[i:]
+
+    else:
+        aux += right[j:]
+    
+    return aux
+
+arr = [12, 11, 13, 5, 6, 7]  
+print(mergesort(arr))
