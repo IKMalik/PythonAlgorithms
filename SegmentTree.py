18790e2400f65be71dc633937d1b1262cd652afc
Ibrahim Malik <32967336+IKMalik@users.noreply.github.com>
2019-10-16 21:46:17 +0100

    Update SegmentTree.py

--- SegmentTree.py
+++ SegmentTree.py
@@ -1,85 +1,81 @@
-class SegmentNode:
-
-  def __init__(self, start, end):
-
-    self.value = 0
-    self.start = start
-    self.end = end
-    self.left = None
-    self.right = None
-
-class SegmentTree:
-
-  def __init__(self, nums):
-
-    self.root = self.buildTree(nums, 0, len(nums)-1)
-
-  
-  def buildTree(self, nums, lo, hi):
-
-    # if given empty list
-    if lo > hi:
-      return None
+class Node:
     
-    #non empty list base case
-    if lo == hi:
-      curr = SegmentNode(lo, hi)
-      curr.value = nums[lo]
-      return curr
+    def __init__(self, lo, hi):
+        
+        self.lo = lo
+        self.hi = hi
+        self.left = None
+        self.right = None
+        self.value = None
+
+
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        
+        if nums is None or len(nums) < 1:
+            return 
+        
+        self.root = self.buildTree(nums, 0, len(nums)-1)
+        
+        
+    def buildTree(self, nums, lo, hi):
+        
+        if lo == hi:
+            node = Node(lo, hi)
+            node.value = nums[hi]
+            return node
+        
+        mid = (hi+lo)//2
+        
+        node = Node(lo, hi)
+        
+        node.left = self.buildTree(nums, lo, mid)
+        node.right = self.buildTree(nums, mid+1, hi)
+        
+        node.value = node.left.value + node.right.value
+        
+        return node
+        
+        
+    def update(self, i: int, val: int) -> None:
+        
+        self.changeVal(self.root, i, val)
     
-    mid = (lo + hi)//2
-
-    curr = SegmentNode(lo, hi)
-    curr.left = self.buildTree(nums, lo, mid)
-    curr.right = self.buildTree(nums, mid+1, hi)
-
-    curr.value = curr.left.value + curr.right.value
-
-    return curr
-  
-  def sumRange(self, lo, hi):
-
-    return self._sumRange(self.root, lo, hi)
-  
-  def _sumRange(self, node, lo, hi):
-
-    # base case
-    if node.start == lo and node.end == hi:
-      return node.value
-
-    mid = (node.start+node.end)//2 # why this instead of hi and lo 
-
-    # 3 cases
-    if hi <= mid:
-      return self._sumRange(node.left, lo, hi)
+    def changeVal(self,node, index, value):
+        
+        if node.lo == node.hi:
+            node.value = value
+            return
+        
+        mid = (node.lo+node.hi)//2
+        
+        if index <= mid:
+            self.changeVal(node.left, index, value)
+        else:
+            self.changeVal(node.right, index, value)
+        
+        node.value = node.left.value + node.right.value
+        return
+        
+    def sumRange(self, i: int, j: int) -> int:
+        
+        return self.rangeSum(self.root, i, j)
     
-    elif lo > mid:
-      return self._sumRange(node.right, lo, hi)
-    
-    else:
-      return self._sumRange(node.left, lo, mid) + self._sumRange(node.right, mid+1, hi)
-
-  def update(self, i, val):
-
-    return self._update(self.root, i, val)
-
-  def _update(self, root, i, val):
-      
-      #Base case
-      if root.start == root.end:
-          root.value = val
-          return 
-  
-      mid = (root.start + root.end) // 2
-      
-      if i <= mid:
-          self._update(root.left, i, val)
-      else:
-          self._update(root.right, i, val)
-      
-      root.value = root.left.value + root.right.value
-
-a = SegmentTree([2,6,8,1,2,3,4])
-print(a.sumRange(0, 3))
-a.update(1, 7)
-print(a.sumRange(0, 3))
+    def rangeSum(self, node, lo, hi):
+        
+        if node.lo == lo and node.hi == hi:
+            return node.value
+        
+        mid = (node.lo+node.hi)//2
+        
+        if lo > mid:
+            return self.rangeSum(node.right, lo, hi)
+        
+        elif hi <= mid:
+            return self.rangeSum(node.left, lo, hi)
+        
+        else:
+            return self.rangeSum(node.left, lo, mid) + self.rangeSum(node.right, mid+1, hi)
+        
+        
