080ae5d596a693aa0a3acde4fb232b1f79ca4d6d
Ibrahim Malik <32967336+IKMalik@users.noreply.github.com>
2021-07-26 16:31:53 +0100

    Update Trie.py

--- Trie.py
+++ Trie.py
@@ -38,3 +38,57 @@ trie.add('bed')
 trie.get('bob')
 trie.get('bed')
 trie.get('be')
+
+
+## Itterating a full trie example
+
+class Node:
+    
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+        self.fullword = None
+
+class Solution:
+    def longestWord(self, words: List[str]) -> str:
+        
+        # add all to trie O(chars)
+        # run through trie to get smallest word O(chars)
+        
+        trie = Node()
+        
+        for word in words:
+            
+            cur = trie
+            
+            for char in word:
+                
+                if char not in cur.children:
+                    cur.children[char] = Node()
+                cur = cur.children[char]
+            cur.isWord = True
+            cur.fullword = word 
+        
+        res = ["", -1]
+        
+        self.itterTrie(trie, res)
+        
+        return res[0]
+        
+        
+    def itterTrie(self, trie, res):
+        
+        for child in trie.children:
+            
+            cur = trie.children[child]
+            
+            if cur.isWord:
+                curLen = len(cur.fullword)
+                
+                if curLen == res[1]:
+                    res[0] = min(res[0], cur.fullword)
+                elif curLen > res[1]:
+                    res[0] = cur.fullword
+                    res[1] = curLen
+                
+                self.itterTrie(cur, res)
